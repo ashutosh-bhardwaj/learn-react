827a67d5b7a6244bc644fc2e8791f0e63339644d
ashutosh bhardwaj <ashutosh.bhardwaj@daffodilsw.com>
2019-01-31 13:10:56 +0530

    Add `Impereative-vs-Declartive`  lesson

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './05-Imperative-to-Declarative';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
